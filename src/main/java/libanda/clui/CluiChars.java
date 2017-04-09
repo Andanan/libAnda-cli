4ec457737cac06eb1f23362e18ccf991ac3d002d
Andanan <Andanan@mail.de>
2017-04-09 21:31:16 +0200

    Created class CluiChars with unimplemented methods

---
+++ src/main/java/libanda/clui/CluiChars.java
@@ -0,0 +1,30 @@
+package libanda.clui;
+
+public class CluiChars {
+
+	public static CluiChars getAsciiCharSet(){
+		return null;
+	}
+	public static CluiChars getSimpleUnicodeCharSet(){
+		return null;
+	}
+	public static CluiChars getDuplexUnicodeCharSet(){
+		return null;
+	}
+	public static CluiChars getMixedUnicodeCharSet(){
+		return null;
+	}
+	
+	public CluiChars() {
+		
+	}
+	public CluiChars(CluiChars template) {
+		
+	}
+	public char get(int key){
+		return '\0';
+	}
+	public void set(int key, char val) {
+		
+	}
+}
