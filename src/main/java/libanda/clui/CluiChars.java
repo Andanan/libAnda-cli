a3f7e6f0190fa93985a5199a1cf6d2e3cbf1c43d
Andanan <Andanan@mail.de>
2017-04-10 18:56:45 +0200

    Updated class CluiChars: added static constants

--- src/main/java/libanda/clui/CluiChars.java
+++ src/main/java/libanda/clui/CluiChars.java
@@ -1,6 +1,21 @@
 package libanda.clui;
 
 public class CluiChars {
+	
+	public static final int TOP_LEFT_CORNER = 0;
+	public static final int TOP_CENTER = 1;
+	public static final int TOP_RIGHT_CORNER = 2;
+	public static final int CENTER_LEFT = 3;
+	public static final int JUNCTION = 4;
+	public static final int CENTER_RIGHT = 5;
+	public static final int BOTTOM_LEFT_CORNER = 6;
+	public static final int BOTTOM_CENTER = 7;
+	public static final int BOTTOM_RIGHT_CORNER = 8;
+	public static final int OUTER_HORIZONTAL = 9;
+	public static final int INNER_HORIZONTAL = 10;
+	public static final int OUTER_VERTICAL = 11;
+	public static final int INNER_VERTICAL = 12;
+	public static final int SPACE = 13;
 
 	public static CluiChars getAsciiCharSet(){
 		CluiChars asciiCharSet = new CluiChars();
