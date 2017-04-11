2ccfff17aea749acbbcbd251aa15899109d7906d
Andanan <Andanan@mail.de>
2017-04-11 05:32:47 +0200

    Class CluiChars: added hashCode(), equals() and toString() methods

--- src/main/java/libanda/clui/CluiChars.java
+++ src/main/java/libanda/clui/CluiChars.java
@@ -153,4 +153,41 @@ public class CluiChars {
 	public void set(int key, char val) {
 		this.charSet.put(key, val);
 	}
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((charSet == null) ? 0 : charSet.hashCode());
+		return result;
+	}
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CluiChars other = (CluiChars) obj;
+		if (charSet == null) {
+			if (other.charSet != null)
+				return false;
+		} else if (!charSet.equals(other.charSet))
+			return false;
+		return true;
+	}
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "CluiChars [charSet=" + charSet + "]";
+	}
+	
 }
