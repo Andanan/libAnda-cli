98eb9cbb744ca951c122ec4bfde08ff0ca01601f
Andanan <Andanan@mail.de>
2017-04-14 14:18:24 +0200

    Created class Controller and class ControllerTest

---
+++ src/main/java/libanda/clui/Controller.java
@@ -0,0 +1,53 @@
+package libanda.clui;
+
+public class Controller {
+
+	public Controller() {
+
+	}
+
+	public Controller(CharMap charMap) {
+
+	}
+
+	public Controller(int width) {
+
+	}
+
+	public Controller(CharMap charMap, int width) {
+
+	}
+
+	public void setWidth(int width) {
+
+	}
+
+	public int getWidth() {
+		return 0;
+	}
+
+	public void setCharMap(CharMap charMap) {
+
+	}
+
+	public CharMap getCharMap() {
+		return new CharMap();
+	}
+
+	public void setPadding(PaddingPosition position, int width) {
+
+	}
+
+	public int getPaddingRight() {
+		return 0;
+	}
+
+	public int getPaddingLeft() {
+		return 0;
+	}
+
+	public void clearPadding(PaddingPosition position) {
+
+	}
+
+}
---
+++ src/test/java/libanda/clui/ControllerTest.java
@@ -0,0 +1,5 @@
+package libanda.clui;
+
+public class ControllerTest {
+
+}
